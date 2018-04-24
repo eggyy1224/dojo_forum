ae0102f8ec56d4092aa0cf2f3dcbde752df901f6
eggyy1224 <eggyy1224@gmail.com>
2018-04-24 10:50:02 +0800

    使用者後台編輯

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,18 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    
+    @users = User.all
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
   end
 end
